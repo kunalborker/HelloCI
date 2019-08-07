0b59e148f0f37eaef935acdbeff09772513b6333
Kunal Borkar <dvb2852@varian.com>
2019-08-07 15:33:48 +0530

    Added New commit to trigger build

--- src/app/HelloCI.Web/Startup.cs
+++ src/app/HelloCI.Web/Startup.cs
@@ -34,7 +34,6 @@ namespace HelloCI.Web
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
